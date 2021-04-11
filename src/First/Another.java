9e4abb8b8b7abf86d0137b05b08edea50b1ac7fa
RahulDwarkani <Rahul.tech495@gmail.com>
2021-04-11 18:36:52 +0530

    added second line

--- src/First/Another.java
+++ src/First/Another.java
@@ -3,6 +3,7 @@ package First;
 public class Another {
 
      void msg() {
+         int a=1;
         System.out.println("Calling another");
          System.out.println("Added Another line");
     }
